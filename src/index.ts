813e3f7c41d8779b299b048eded354c45fc1af42
DonovanYe <xxiaozhiyzj@gmail.com>
2024-09-10 18:51:38 +0800

    feat: init entry of the project

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-export const one = 1
-export const two = 2
+#!/usr/bin/env node
+
+import process from 'node:process'
+import { Command } from 'commander'
+import { getPackageInfo } from '../utils/get-package-info'
+
+process.on('SIGINT', () => process.exit(0))
+process.on('SIGTERM', () => process.exit(0))
+
+async function main(): Promise<void> {
+  const packageInfo = getPackageInfo()
+
+  const program = new Command()
+    .name(packageInfo.name!)
+    .description(packageInfo.description!)
+    .version(
+      packageInfo.version!,
+      '-v, --version',
+      'display the version number',
+    )
+
+  // program.addCommand(init).addCommand(add).addCommand(diff).addCommand(pull).addCommand(push).addCommand(pushBusiness)
+
+  program.parse()
+}
+
+main()
